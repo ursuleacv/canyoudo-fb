951a0e0a40a936211f4f8410382066537ddfa0bd
Valentin Ursuleac <ursuleacv@gmail.com>
2013-08-05 17:32:51 -0400

    Added headers

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With');
 
 /**
  * This sample app is provided to kickstart your experience using Facebook's
